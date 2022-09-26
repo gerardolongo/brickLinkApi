d352f13f6b4fac38a1dcff1f3f934d5403842bb1
gerardolongo <gerardo.longo1@gmail.com>
2022-09-26 20:45:35 +0200

    Update BrickLinkController.cs

--- Controllers/BrickLinkController.cs
+++ Controllers/BrickLinkController.cs
@@ -10,7 +10,7 @@ namespace brickLinkApi.Controllers
     public class BrickLinkController : ControllerBase
     {
         //[EnableCors("AllowOrigin")]
-        [EnableCors(origins: "https://bricklinkapp.herokuapp.com", headers: "*", methods: "*")]
+        [EnableCors("https://bricklinkapp.herokuapp.com", headers: "*", methods: "*")]
         [HttpGet("{numItem}")]
         public string Get(string numItem)
         {
